4ebe2abff22fd22f937cbb839e248a435b7e34b0
MaetDol <rndka9@gmail.com>
2022-10-10 12:19:03 +0900

    특정 slug 로 포스트를 가져오는 api 작성

---
+++ pages/api/posts/[slug].ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import postService from '../__services/post.service';
+
+/**
+ * /api/posts/[slug]
+ *  slug 에 해당하는 포스트를 가져옵니다
+ */
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const slug = req.query.slug?.toString() || '';
+  const post = postService.getPost(slug);
+
+  if (!post) {
+    res.status(404).send('Post not found');
+    return;
+  }
+
+  res.status(200).json(post);
+}
