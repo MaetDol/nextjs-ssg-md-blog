0c7e50a2de5097901446ef47c055e4bb44e8f9f3
MaetDol <rndka9@gmail.com>
2022-10-10 12:02:38 +0900

    포스트 리스트를 가져오는 api 작성

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import postService from '../__services/post.service';
+
+/**
+ * /api/posts
+ * 모든 포스트의 메타 데이터를 가져옵니다
+ */
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const postMetadatas = postService.getAllPostMetaData();
+  res.status(200).json(postMetadatas);
+}
