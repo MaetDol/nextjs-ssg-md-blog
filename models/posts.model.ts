3c535bcf0fe6916e9d8bc423c4e0562d58c51102
MaetDol <rndka9@gmail.com>
2022-10-10 11:50:26 +0900

    포스트 메타데이터 타입 선언

---
+++ models/posts.model.ts
@@ -0,0 +1,8 @@
+export type PostMetaData = {
+  categories: string | string[];
+  date: `${number}-${number}-${number}`;
+  description: string;
+  slug: string;
+  tags: string | string[];
+  title: string;
+};
