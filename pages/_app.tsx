f61bc48217f502c4994c597a35f0b55c15591cb7
MaetDol <rndka9@gmail.com>
2022-10-14 04:22:19 +0900

    최상위 컴포넌트 SWRConfig 래핑
    앱 전역에서 캐싱을 적용할 수 있도록 래핑했어요

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig>
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
