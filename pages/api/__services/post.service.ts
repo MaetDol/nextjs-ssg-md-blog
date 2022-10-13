13da2a7e2346c143c36a6cb52b0b1da00fcf45f1
MaetDol <rndka9@gmail.com>
2022-10-14 06:52:45 +0900

    Vercel 배포시 로컬 파일 포함하게 하기 위해 path.join 구문 추가

--- pages/api/__services/post.service.ts
+++ pages/api/__services/post.service.ts
@@ -1,9 +1,14 @@
-import { readFileSync } from "fs";
+import path from "path";
 import { PostMetaData } from "../../../models/posts.model";
 import { readDirSyncAtLocal, readFileSyncAtLocal } from "../__utils/file.utils";
 import { getYamlMetaData, parseToAST } from "../__utils/remark.utils";
 
 const POST_DIRECTORY = "__posts";
+// @vercel/nft 의 미숙한 동작 때문에, 동적으로(폴더를 읽고, 거기서 파일 목록을 가져오는 등) 파일을 불러오는 경우
+// 로컬 파일이 복사되지 않고 누락되는 케이스가 있습니다
+// 그 경우 아래와 같이 path.join 명령행을 추가해, 파일 위치를 알 수 있게 명시해주니 잘 복사되네요
+// https://github.com/vercel/next.js/discussions/32236#discussioncomment-3029649
+path.join(process.cwd(), POST_DIRECTORY);
 
 /**
  *  모든 포스트의 메타데이터를 가져옵니다
