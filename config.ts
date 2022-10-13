5bc063a49af249bfc060dafa77d1a503257e8e20
MaetDol <rndka9@gmail.com>
2022-10-14 04:20:13 +0900

    별도의 config 파일 대신, next.config.js 를 활용하게 변경
    basePath 값을 옮겼어요

--- config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  server_url:
-    process.env.NODE_ENV === 'production'
-      ? 'https://maetdol.github.io/nextjs-ssg-md-blog'
-      : 'http://localhost:3000',
-} as const;
-
-export default config;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+const BASE_PATH = "nextjs-ssg-md-blog";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/nextjs-ssg-md-blog',
-  assetPrefix: '/nextjs-ssg-md-blog/',
+  basePath: `/${BASE_PATH}`,
+  assetPrefix: `/${BASE_PATH}/`,
+  env:{
+    basePath: BASE_PATH
+  }
 };
 
 module.exports = nextConfig;
